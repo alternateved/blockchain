e1fc43c73f766ec8033fdcc511c6375051db5ba4
alternateved <alternateved@gmail.com>
2021-05-09 19:58:41 +0200

    Create /transaction endpoint

--- dev/api.js
+++ dev/api.js
@@ -1,9 +1,18 @@
 const express = require('express');
-
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: false, limit: '20mb' }));
+
+app.get('/blockchain', function (req, res) {});
+
+app.post('/transaction', function (req, res) {
+  console.log(req.body);
+  res.send(`The amount of the transaction is ${req.body.amount} bitcoin.`);
 });
 
-app.listen(3000);
+app.get('/mine', function (req, res) {});
+
+app.listen(3000, function () {
+  console.log('listening on port 3000...');
+});
