cb53adba1c7deed883e519da390fa3effda9b3e3
alternateved <alternateved@gmail.com>
2021-05-09 19:59:05 +0200

    Create /blockchain endpoint

--- dev/api.js
+++ dev/api.js
@@ -1,10 +1,15 @@
+const Blockchain = require('./blockchain');
 const express = require('express');
 const app = express();
+
+const bitcoin = new Blockchain();
 
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ extended: false, limit: '20mb' }));
 
-app.get('/blockchain', function (req, res) {});
+app.get('/blockchain', function (req, res) {
+  res.send(bitcoin);
+});
 
 app.post('/transaction', function (req, res) {
   console.log(req.body);
